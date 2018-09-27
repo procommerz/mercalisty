391126267ff009339542932d9ed4028e89e51953
Denis Kolesnichenko <denis@farmy.ch>
2018-09-27 09:09:05 +0200

    no message

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ActiveRecord::Base
   belongs_to :product
-  belongs_to :brand, through: :product
+  # belongs_to :brand, through: :product
 
   def agent
     agent_id ? agent_id.constantize.new : nil
